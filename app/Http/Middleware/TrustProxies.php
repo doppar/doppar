c027d8bdd01c1f3a6a75bd26f15a997684f046c5
Arif Hoque <arif@sulaco-tec.com>
2025-05-18 22:08:47 +0600

    trusted proxies middleware updated:

--- app/Http/Middleware/TrustProxies.php
+++ app/Http/Middleware/TrustProxies.php
@@ -3,12 +3,15 @@
 namespace App\Http\Middleware;
 
 use Phaseolies\Middleware\Contracts\Middleware;
+use Phaseolies\Http\Support\InteractsWithTrustedProxies;
 use Phaseolies\Http\Response;
 use Phaseolies\Http\Request;
 use Closure;
 
 class TrustProxies implements Middleware
 {
+    use InteractsWithTrustedProxies;
+
     /**
      * The trusted proxies for this application
      *
@@ -48,85 +51,5 @@ class TrustProxies implements Middleware
         );
 
         return $next($request);
-    }
-
-    /**
-     * Get the trusted proxies
-     *
-     * @return array
-     */
-    protected function getTrustedProxies(): array
-    {
-        if (is_string($this->proxies)) {
-            return $this->resolveSpecialProxies($this->proxies);
-        }
-
-        return $this->proxies;
-    }
-
-    /**
-     * Resolve special proxy values
-     *
-     * @param string $proxies
-     * @return array
-     */
-    protected function resolveSpecialProxies(string $proxies): array
-    {
-        switch ($proxies) {
-            case '*':
-                return ['*'];
-
-            case '**':
-                return $this->privateSubnets;
-
-            default:
-                return [$proxies];
-        }
-    }
-
-    /**
-     * Get the proxy header configuration
-     *
-     * @return int
-     */
-    protected function getTrustedHeaders(): int
-    {
-        return $this->headers;
-    }
-
-    /**
-     * Set the trusted proxies
-     *
-     * @param array|string $proxies
-     * @return self
-     */
-    public function setProxies($proxies): self
-    {
-        $this->proxies = $proxies;
-        return $this;
-    }
-
-    /**
-     * Set the trusted headers
-     *
-     * @param int $headers
-     * @return self
-     */
-    public function setHeaders(int $headers): self
-    {
-        $this->headers = $headers;
-        return $this;
-    }
-
-    /**
-     * Set private subnets
-     *
-     * @param array $subnets
-     * @return self
-     */
-    public function setPrivateSubnets(array $subnets): self
-    {
-        $this->privateSubnets = $subnets;
-        return $this;
     }
 }
