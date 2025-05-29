29caa107a6521ea13292573c3452662a01c1f2fc
Arif Hoque <arif@sulaco-tec.com>
2025-05-29 17:44:58 +0600

    background cron issue resolved:

--- config/filesystem.php
+++ config/filesystem.php
@@ -58,5 +58,7 @@ return [
 
     'links' => [
         public_path('storage') => storage_path('app/public'),
+        // add another like
+        // public_path('avatars') => storage_path('app/avatars'),
     ],
 ];
