0d16f3310dbc57c762f4578958e65a5194d101f7
Arif Hoque <arif@sulaco-tec.com>
2025-07-25 15:30:13 +0600

    auth model dynamic

---
+++ config/auth.php
@@ -0,0 +1,8 @@
+<?php
+
+// This is the model used for authentication in Doppar.
+// By default, it uses App\Models\User, but the user can change this
+// to point to a different model if custom authentication logic is needed.
+return [
+    'model' => App\Models\User::class
+];
